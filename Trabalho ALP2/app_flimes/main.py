ad2231e23f79f7d4454ec8d65df7012b9c050758
MosiasFrtizCarmo <lethalporto@gmail.com>
2025-11-10 23:58:46 -0300

    Add options for reading and deleting films

--- Trabalho ALP2/app_flimes/main.py
+++ Trabalho ALP2/app_flimes/main.py
@@ -5,7 +5,7 @@ filme_series = []
 
 while True:
     print("\nMenu:\n")
-    print("[1] Cadastrar filme/serie [2] Atualizar filme")
+    print("[1] Cadastrar filme/serie [2] Atualizar filme [3]Leitura dos filmes  [4]Deletar um filme")
     print("[0] Sair")
 
     opcao = input("\nOpção: ")
@@ -14,6 +14,10 @@ while True:
         app_flimes.adicionar(filme_series)
     elif opcao == "2":
         app_flimes.atualizar(filme_series)
+    elif opcao == "3":
+        app_flimes.ler(filme_series)
+    elif opcao == "4":
+        app_flimes.deletar(filme_series)
     elif opcao == "0":
         print("\nAdeus...")
         break
